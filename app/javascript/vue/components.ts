251871d722667b4dbbbd21c9a4e80a5cc13e7f6b
Franta1205 <hoza.fr@gmail.com>
2025-04-27 20:07:37 +0200

    nav

--- app/javascript/vue/components.ts
+++ app/javascript/vue/components.ts
@@ -1,7 +1,9 @@
 import Greeting from './components/Greeting.vue'
+import SidebarNavigation from './admin/navigation/SidebarNavigation.vue'
 
 const components = {
   'Greeting': Greeting,
+  'SidebarNavigation': SidebarNavigation,
 }
 
 export function getComponent(name) {
